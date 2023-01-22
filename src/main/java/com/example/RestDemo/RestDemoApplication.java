7ba328fdb900a31f6e97b1a2956e3609092aad76
srivatzm <srivatzm@che>
2023-01-22 16:33:13 +0000

    GET changes

--- src/main/java/com/example/RestDemo/RestDemoApplication.java
+++ src/main/java/com/example/RestDemo/RestDemoApplication.java
@@ -1,11 +1,22 @@
 package com.example.RestDemo;
 
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class RestDemoApplication {
 
+	@GetMapping("/prods")
+	List<String> getProds() {
+		List<String> list = new ArrayList<>();
+		list.add("Test");
+		list.add("Geo");
+		return list;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestDemoApplication.class, args);
 	}
